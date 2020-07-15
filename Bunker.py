5717e879d312ab3daab467b58566cddea2eb94ae
Volume999 <Cholponbaev_a@auca.kg>
2020-07-15 22:50:16 +0600

    Excel file with data

--- Bunker.py
+++ Bunker.py
@@ -3,34 +3,113 @@ from pprint import pprint
 import re
 from collections import defaultdict
 import csv
+import openpyxl
+import pandas as pd
+import xlsxwriter
 
-class Jobs:
-    names = list()
-    descriptions = defaultdict(str)
 
-    def __init__(self):
+class GeneratorEngine:
+    jobs = list()
+    diseases = list()
+    hobbies = list()
+    human_traits = list()
+    phobias = list()
+    catastrophes = list()
+    wiki = wikipediaapi.Wikipedia("ru")
+
+    @staticmethod
+    def get_from_wiki_by_category(category):
+        ans = list()
+        wiki = wikipediaapi.Wikipedia("ru")
+        page_py = wiki.page(category)
+        for page in page_py.categorymembers:
+            pattern = r':'
+            if not re.search(pattern, page):
+                summary = wiki.page(page).summary
+                # if len(summary) < 400:
+                ans.append((page, summary))
+        return ans
+
+    def init_jobs_wiki(self):
+        self.jobs = self.get_from_wiki_by_category("Категория:Профессии")
+
+    def init_health_wiki(self):
+        self.diseases = self.get_from_wiki_by_category("Категория:Заболевания_по_алфавиту")
+
+    def init_biological_characteristics(self):
+        pass
+
+    def init_additional_skills(self):
+        pass
+
+    def init_human_character(self):
+        self.human_traits = self.get_from_wiki_by_category("Категория:Черты_личности")
+
+    def init_hobbies(self):
+        self.hobbies = self.get_from_wiki_by_category("Категория:Хобби")
+
+    def init_phobias(self):
+        page = self.wiki.page("Список_фобий")
+        # print(page.sections)
+        letter = "А"
+        for i in range(32):
+            section = page.section_by_title(letter)
+            if section:
+                lines = section.text.split(sep='\n')
+                for phobia in lines:
+                    if len(phobia) > 0:
+                        name, summary = phobia.split(sep=' — ', maxsplit=1)
+                        self.phobias.append((name, summary))
+            letter = chr(ord(letter) + 1)
+
+    def init_special_conditions(self):
+        pass
+
+    def init_catastrophe(self):
+        categories = ("Категория:Природные_катастрофы", "Категория:Техногенные_катастрофы", "Категория:Экологические_катастрофы")
+        for category in categories:
+            self.catastrophes += self.get_from_wiki_by_category(category)
+
+
+class Game:
+    class Player:
+        pass
+
+    class Context:
         pass
 
 
-def initialize_jobs():
-    jobs = Jobs()
-    wiki = wikipediaapi.Wikipedia("ru")
-    page_py = wiki.page("Категория:Профессии")
-    # pprint(page_py.categorymembers)
-    for page in page_py.categorymembers:
-        pattern = r':'
-        if not re.search(pattern, page):
-            summary = wiki.page(page).summary
-            jobs.names.append(page)
-            jobs.descriptions[page] = summary
-    for i in jobs.names:
-        print(i)
-        print(jobs.descriptions[i])
-    return jobs
-
-
 def main():
-    jobs = initialize_jobs()
+    generator = GeneratorEngine()
+    generator.init_jobs_wiki()
+    generator.init_health_wiki()
+    generator.init_human_character()
+    generator.init_hobbies()
+    generator.init_phobias()
+    generator.init_catastrophe()
+    with pd.ExcelWriter("Bunker.xlsx", engine='xlsxwriter') as writer:
+        files = {
+            "Jobs": generator.jobs,
+            "Health": generator.diseases,
+            "Human Character Traits": generator.human_traits,
+            "Hobbies": generator.hobbies,
+            "Phobias": generator.phobias,
+            "Catastrophes": generator.catastrophes
+        }
+        for (sheet_name, data) in files.items():
+            # print(data)
+            df = pd.DataFrame(data, columns=["Name", "Description"])
+            df.to_excel(writer, sheet_name=sheet_name)
 
 
 main()
+
+# wiki = wikipediaapi.Wikipedia("ru")
+# page_py = wiki.page("Категория:Черты_личности")
+# for page in page_py.categorymembers:
+#     pattern = r':'
+#     if not re.search(pattern, page):
+#         summary = wiki.page(page).summary
+#         # if len(summary) < 400:
+#         # ans.append((page, summary))
+#         print(page, summary)
